abe382876bd620f1b3023e1c2a3e083a2543342f
hanoolchoi75 <54473918+hanoolchoi75@users.noreply.github.com>
2019-08-24 23:19:26 +0900

    Update hello.py for Pull Example

--- hello.py
+++ hello.py
@@ -1,6 +1,8 @@
 // For command line git tutorial
+// For Pull Example, which is created in the GitHub
 
 print("Hello World")
 print("Tell your world")
 print("Tell his world")
 print("Tell your Sister")
+print("Tell yourself")
